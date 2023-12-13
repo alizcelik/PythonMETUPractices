e5f8be6d7354331edd35f09f6d10b27e5cc10863
alizcelik <ali.ozcelik_01@metu.edu.tr>
2023-12-14 00:12:37 +0300

    class

--- youtube.course/OOP/ClassVariables.py
+++ youtube.course/OOP/ClassVariables.py
@@ -1,5 +1,8 @@
 # Example Class
 class Employee:
+    #class variable
+    raise_amount = 1.04
+    num_of_emps= 0
 
     # init method -> constructor
     def __init__(self, first, last, pay):
@@ -8,9 +11,36 @@ class Employee:
         self.pay = pay
         self.email = str.lower(first) + '.' + str.lower(last) + '@company.com'
 
+        Employee.num_of_emps += 1
+
     # class method
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
-# Instance -> Without init
+
+# Instance -> With init
+emp_1 = Employee('Phyton', 'OOP', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+print(Employee.num_of_emps)
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+emp_1.raise_amount = 1.05
+Employee.raise_amount = 1.06
+
+#Instance Variables
+print(emp_1.raise_amount)
+
+
+#Class Variables
+print(Employee.raise_amount)
